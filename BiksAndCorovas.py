d66f8462befef2121b59fe819adb64257b6eddcb
Daniar <daniar670@mail.ru>
2024-11-06 15:36:16 +0300

    V0.1.1, added time statistic, fixed bugs

---
+++ BiksAndCorovas.py
@@ -0,0 +1,64 @@
+import random
+import os
+class BiksAndCorovas():
+    def __init__(self, login=''):
+        if login=='':
+            self.login=input("Who are you, warrior?\n")
+        self.number_of_digits=int(input("How many IQ do you think you have?\n"))
+        self.number_of_steps=0
+        while self.number_of_digits>9 or self.number_of_digits<1:
+            print("Error: The IQ level must be between 1 and 9.")
+            self.number_of_digits=int(input("Please try again.\n"))
+        self.word=self.generate_word(self.number_of_digits)
+        self.time=0.1
+    
+    def save_result(self):
+        if not os.path.exists(self.login):
+            os.mkdir(self.login)
+        with open(self.login + '/' + str(self.number_of_digits) + '.txt', 'a') as file:
+            file.write(f'{self.number_of_steps} / {self.word} / {self.time}\n')
+
+    def generate_word(self, number_of_digits):
+        word=''
+        for i in range(number_of_digits):
+            digit = random.randint(0,9)
+            while str(digit) in word:
+                digit = random.randint(0,9)
+            word+=str(digit)
+        return word
+
+    def fool_protection(self, user_guess):
+        if user_guess=='im dumb':
+            print(f"ok u lil cheater -__- here is the number i guessed: {self.word}")
+            print("////////////////////////////////////////////////////////////////////")
+            return False
+        if len(user_guess)!=len(self.word):
+            print("Error: The guess must be the same length as the word.")
+            return True
+        for i in user_guess:
+            if not (i in ['0','1','2','3','4','5','6','7','8','9']):
+                print("Error: The guess must only contain digits.")
+                return True
+        return False
+
+    def guess_word(self):
+        self.number_of_steps+=1
+        user_guess=input("Ok show me you IQ lvl: \n")
+        while self.fool_protection(user_guess):
+            user_guess=input('cringe but ok, try again...\n')
+
+        biks=0
+        corovas=0
+        for i in range(len(self.word)):
+            if self.word[i]==user_guess[i]:
+                biks+=1
+            elif user_guess[i] in self.word:
+                corovas+=1
+
+        if user_guess==self.word:
+            print("Congratulations! You guessed the word correctly.\n")
+            return True
+        else:
+            print("You didn't get it right, but here are your tips:\n" + "Number of biks: " + str(biks) + "\nNumber of corovas: " + str(corovas) + '\n')
+            return False
+
---
+++ main.py
@@ -0,0 +1,79 @@
+from BiksAndCorovas import BiksAndCorovas
+import time
+def main(login=''):
+    game=BiksAndCorovas(login)
+    start_time=time.time()
+    while not game.guess_word():
+        pass
+    game.time=round(time.time() - start_time,2)
+    print(f'you did it in {game.number_of_steps} steps, and it took {game.time} seconds. Do you want to save your result? y/n')
+    if input()=='y':
+        game.save_result()
+
+    print ('do you want to show your stats on this IQ level? y/n')
+
+    if input()=='y':
+        with open(game.login + '/' + str(game.number_of_digits) + '.txt', 'r') as file:
+            strings = file.readlines()
+
+            sum = 0
+            hardest_word=''
+            hardest_word_guesses=0
+            hardest_word_time = 0.1
+
+            
+            
+            easiest_word=''
+            easiest_word_guesses=10000
+            easiest_word_time=10000000
+
+            shortest_time=10000000
+            shortest_time_word=''
+            shortest_time_word_guesses=1
+            longest_time=0.1
+            longest_time_word=''
+            longest_time_word_guesses=1
+
+            for i in range(len(strings)):
+                
+                string=strings[i].strip()
+                number_of_guesses=int(string[:string.find(' '):])
+                word_space=string.find(' ')
+                time_space=string.find(' ', word_space+3)
+                time_taken=float(string[time_space+3:])
+
+                if number_of_guesses>hardest_word_guesses:
+                    hardest_word_guesses=number_of_guesses
+                    hardest_word=string[word_space+3:word_space+3+game.number_of_digits:]
+                    hardest_word_time=time_taken
+                if number_of_guesses<easiest_word_guesses:
+                    easiest_word_guesses=number_of_guesses
+                    easiest_word=string[word_space+3:word_space+3+game.number_of_digits:]
+                    easiest_word_time=time_taken
+                
+                if time_taken>longest_time:
+                    longest_time=time_taken
+                    longest_time_word=string[word_space+3:word_space+3+game.number_of_digits:]
+                    longest_time_word_guesses=number_of_guesses
+                if time_taken<shortest_time:
+                    shortest_time=time_taken
+                    shortest_time_word=string[word_space+3:word_space+3+game.number_of_digits:]
+                    shortest_time_word_guesses=number_of_guesses
+
+                sum += number_of_guesses
+
+        middle_result=sum/len(strings)
+        
+        print(f'Amount of attempts is {len(strings)}\nThe middle result is {middle_result}'
+            f'\nThe hardest word ever is {hardest_word} with {hardest_word_guesses} guesses in {hardest_word_time} seconds'
+            f'\nThe easiest word ever is {easiest_word} with {easiest_word_guesses} guesses in {easiest_word_time} seconds'
+            f'\nThe longest time taken is {longest_time} seconds for the word {longest_time_word} in {longest_time_word_guesses} guesses'
+            f'\nThe shortest time taken is {shortest_time} seconds for the word {shortest_time_word} in {shortest_time_word_guesses} guesses')
+        
+    print('----------------------------------------------------------------')
+    print('Wanna play again? y/n')
+
+    if input()=='y':
+        main(game.login)
+
+main()
